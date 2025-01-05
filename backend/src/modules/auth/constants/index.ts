683112561c425b75b3796ae3e2ee0d0270d6e8fa
ziebamikolaj <ziebamikolaj03@gmail.com>
2025-01-05 03:58:18 +0100

    fix(API): :ambulance: branch merge fix

--- backend/src/modules/auth/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export const MAX_EMAIL_LENGTH = 60;
-export const MAX_PASSWORD_LENGTH = 50;
-
-export const JWT_AGE_IN_DAYS = 14;
-export const JWT_AGE_IN_SECONDS = JWT_AGE_IN_DAYS * 24 * 60 * 60;
--- backend/src/modules/utils/error-messages.ts
+++
@@ -1,5 +0,0 @@
-export const errorMessages = {
-  BAD_REQUEST: "Bad request",
-  UNAUTHORIZED: "User unauthorized",
-  USER_EXIST: "User already exists",
-} as const;
